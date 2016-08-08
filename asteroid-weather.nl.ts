3457d3df7acab92ba666ba6c51c48891d74a14b7
Sylvia van Os <iamsylvie@openmailbox.org>
2016-08-08 17:10:36 +0200

    Add Dutch translation

---
+++ asteroid-weather.nl.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="42"/>
+        <source>Unknown</source>
+        <translation>Onbekend</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="75"/>
+        <source>Today</source>
+        <translation>Vandaag</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="78"/>
+        <source>Tomorrow</source>
+        <translation>Morgen</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="120"/>
+        <source>Min:</source>
+        <translation>Min:</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="120"/>
+        <location filename="main.qml" line="142"/>
+        <source>°C</source>
+        <translation>°C</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="142"/>
+        <source>Max:</source>
+        <translation>Max:</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="194"/>
+        <source>&lt;h3&gt;No data&lt;/h3&gt;
+Sync AsteroidOS with your phone.</source>
+        <translation>&lt;h3&gt;Geen data&lt;/h3&gt;
+Synchroniseer AsteroidOS met je telefoon.</translation>
+    </message>
+</context>
+</TS>
