a1a4feda25deb06611ad83013f1714c8854a62ea
Juraj Perenyi <gyperenyi@yahoo.com>
2016-08-12 09:18:29 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (8 of 8 strings)

--- i18n/asteroid-weather.sk.ts
+++ i18n/asteroid-weather.sk.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="asteroid-weather.desktop.h" line="5"/>
         <source>Weather</source>
-        <translation type="unfinished"></translation>
+        <translation>Počasie</translation>
     </message>
 </context>
 <context>
