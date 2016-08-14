519c6748612f0a5ed35b3b84e6c0e02339aa6d8d
A <hucul@o2.pl>
2016-08-14 09:02:27 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (7 of 7 strings)

--- asteroid-weather.pl.ts
+++ asteroid-weather.pl.ts
@@ -6,39 +6,40 @@
     <message>
         <location filename="main.qml" line="42"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Nieznany</translation>
     </message>
     <message>
         <location filename="main.qml" line="75"/>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>Dzisiaj</translation>
     </message>
     <message>
         <location filename="main.qml" line="78"/>
         <source>Tomorrow</source>
-        <translation type="unfinished"></translation>
+        <translation>Jutro</translation>
     </message>
     <message>
         <location filename="main.qml" line="120"/>
         <source>Min:</source>
-        <translation type="unfinished"></translation>
+        <translation>Min.:</translation>
     </message>
     <message>
         <location filename="main.qml" line="120"/>
         <location filename="main.qml" line="142"/>
         <source>°C</source>
-        <translation type="unfinished"></translation>
+        <translation>°C</translation>
     </message>
     <message>
         <location filename="main.qml" line="142"/>
         <source>Max:</source>
-        <translation type="unfinished"></translation>
+        <translation>Max.:</translation>
     </message>
     <message>
         <location filename="main.qml" line="194"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Brak danych&lt;/h3&gt;
+Synchronizacja AsteroidOS z twoim telefonem.</translation>
     </message>
 </context>
 </TS>
