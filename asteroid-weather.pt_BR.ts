79513d21af0421914d7ac6a62f53ce5cf895d09e
Douglas Araújo <dodopokcamilo@gmail.com>
2016-12-08 18:11:44 +0000

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 85.7% (7 of 7 strings)

--- asteroid-weather.pt_BR.ts
+++ asteroid-weather.pt_BR.ts
@@ -38,7 +38,7 @@
         <location filename="main.qml" line="194"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished">&lt;h3&gt;Sem dados&lt;/h3&gt;
+        <translation>&lt;h3&gt;Sem dados&lt;/h3&gt;
 Sincronize o AsteroidOS com o telefone.</translation>
     </message>
 </context>
