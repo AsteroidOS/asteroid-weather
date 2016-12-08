b200946d2a0b2e6f6274fb0524d3bf2784cbf6eb
HASAN ERYILMAZ <hasaneryilmaz0@gmail.com>
2016-12-08 23:55:59 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (7 of 7 strings)

--- asteroid-weather.tr.ts
+++ asteroid-weather.tr.ts
@@ -6,39 +6,40 @@
     <message>
         <location filename="main.qml" line="42"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Bulunamadı</translation>
     </message>
     <message>
         <location filename="main.qml" line="75"/>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>Bugün</translation>
     </message>
     <message>
         <location filename="main.qml" line="78"/>
         <source>Tomorrow</source>
-        <translation type="unfinished"></translation>
+        <translation>Yarın</translation>
     </message>
     <message>
         <location filename="main.qml" line="120"/>
         <source>Min:</source>
-        <translation type="unfinished"></translation>
+        <translation>En Az:</translation>
     </message>
     <message>
         <location filename="main.qml" line="120"/>
         <location filename="main.qml" line="142"/>
         <source>°C</source>
-        <translation type="unfinished"></translation>
+        <translation>°C</translation>
     </message>
     <message>
         <location filename="main.qml" line="142"/>
         <source>Max:</source>
-        <translation type="unfinished"></translation>
+        <translation>En Çok:</translation>
     </message>
     <message>
         <location filename="main.qml" line="194"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Veri Yok&lt;/h3&gt;
+AsteroidOS'u telefonunuzla senkronize edin.</translation>
     </message>
 </context>
 </TS>
