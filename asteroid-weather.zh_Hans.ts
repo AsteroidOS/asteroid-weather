33ea1b835856f224d21a27a6b27b01997dce995a
徐杨（Seven小杨） <xy19009188@gmail.com>
2016-12-08 07:24:03 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 100.0% (7 of 7 strings)

--- asteroid-weather.zh_Hans.ts
+++ asteroid-weather.zh_Hans.ts
@@ -6,39 +6,40 @@
     <message>
         <location filename="main.qml" line="42"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>未知</translation>
     </message>
     <message>
         <location filename="main.qml" line="75"/>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>今天</translation>
     </message>
     <message>
         <location filename="main.qml" line="78"/>
         <source>Tomorrow</source>
-        <translation type="unfinished"></translation>
+        <translation>明天</translation>
     </message>
     <message>
         <location filename="main.qml" line="120"/>
         <source>Min:</source>
-        <translation type="unfinished"></translation>
+        <translation>最低：</translation>
     </message>
     <message>
         <location filename="main.qml" line="120"/>
         <location filename="main.qml" line="142"/>
         <source>°C</source>
-        <translation type="unfinished"></translation>
+        <translation>°C</translation>
     </message>
     <message>
         <location filename="main.qml" line="142"/>
         <source>Max:</source>
-        <translation type="unfinished"></translation>
+        <translation>最高：</translation>
     </message>
     <message>
         <location filename="main.qml" line="194"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt; 1无数据&lt;/h3&gt; 2
+和手机同步中.</translation>
     </message>
 </context>
 </TS>
