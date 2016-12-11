bd0afc359d0a2202e5f5ad372cd3ff5e0e1f2f25
Khaleel Jageer <jskcse4@gmail.com>
2016-12-11 12:07:48 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (7 of 7 strings)

--- asteroid-weather.ta.ts
+++ asteroid-weather.ta.ts
@@ -6,39 +6,40 @@
     <message>
         <location filename="main.qml" line="42"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>அறியப்படாத</translation>
     </message>
     <message>
         <location filename="main.qml" line="75"/>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>இன்று</translation>
     </message>
     <message>
         <location filename="main.qml" line="78"/>
         <source>Tomorrow</source>
-        <translation type="unfinished"></translation>
+        <translation>நாளை</translation>
     </message>
     <message>
         <location filename="main.qml" line="120"/>
         <source>Min:</source>
-        <translation type="unfinished"></translation>
+        <translation>குறைந்தபட்சம்:</translation>
     </message>
     <message>
         <location filename="main.qml" line="120"/>
         <location filename="main.qml" line="142"/>
         <source>°C</source>
-        <translation type="unfinished"></translation>
+        <translation>°செ</translation>
     </message>
     <message>
         <location filename="main.qml" line="142"/>
         <source>Max:</source>
-        <translation type="unfinished"></translation>
+        <translation>அதிகபட்சம்:</translation>
     </message>
     <message>
         <location filename="main.qml" line="194"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;தரவு இல்லை&lt;/h3&gt;
+AsteroidOS-ஐ உங்கள் சாதனத்துடன் ஒத்திசைக்கவும்.</translation>
     </message>
 </context>
 </TS>
