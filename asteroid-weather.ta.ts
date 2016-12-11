7126a6f46d18339b46365936f308604c7d47b846
Khaleel Jageer <jskcse4@gmail.com>
2016-12-11 11:36:33 +0000

    Added translation using Weblate (Tamil)

---
+++ asteroid-weather.ta.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ta">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="42"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="75"/>
+        <source>Today</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="78"/>
+        <source>Tomorrow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="120"/>
+        <source>Min:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="120"/>
+        <location filename="main.qml" line="142"/>
+        <source>°C</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="142"/>
+        <source>Max:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="194"/>
+        <source>&lt;h3&gt;No data&lt;/h3&gt;
+Sync AsteroidOS with your phone.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
