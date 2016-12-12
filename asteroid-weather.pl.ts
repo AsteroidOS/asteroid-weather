6bef527044881d46b1d532db34e9f41427db36e7
Bartosz Gadzała <bartosz.gadzala@gmail.com>
2016-12-12 13:07:30 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (7 of 7 strings)

--- asteroid-weather.pl.ts
+++ asteroid-weather.pl.ts
@@ -38,8 +38,8 @@
         <location filename="main.qml" line="194"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished">&lt;h3&gt;Brak danych&lt;h3&gt;
-Wykonaj synchronizację z telefonem.</translation>
+        <translation>&lt;h3&gt;Brak danych&lt;/h3&gt;
+Zsynchronizuj AsteroidOS  z telefonem.</translation>
     </message>
 </context>
 </TS>
