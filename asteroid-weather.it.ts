c148819aea7a89d76af8ec2831fc697b1236d948
dr_gogeta86 <dr_gogeta86@messinalug.org>
2016-12-13 08:52:23 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (7 of 7 strings)

--- asteroid-weather.it.ts
+++ asteroid-weather.it.ts
@@ -38,7 +38,7 @@
         <location filename="main.qml" line="194"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation>&lt;h3&gt;No data&lt;/h3&gt;
+        <translation>&lt;h3&gt;Nessun dato&lt;/h3&gt;
 Sincronizza AsteroidOS con il telefono.</translation>
     </message>
 </context>
