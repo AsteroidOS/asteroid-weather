3bd7265975d49252d14e83b774dc2fac42b2a937
tytan652 <tytan652@tytanium.xyz>
2016-12-25 12:13:33 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (7 of 7 strings)

--- asteroid-weather.fr.ts
+++ asteroid-weather.fr.ts
@@ -39,7 +39,7 @@
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
         <translation>&lt;h3&gt;Pas de données&lt;/h3&gt;
-Sync AsteroidOS avec votre téléphone.</translation>
+Synchronisez AsteroidOS avec votre téléphone.</translation>
     </message>
 </context>
 </TS>
