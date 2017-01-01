e7e1511ee35a704e1e2d5725b52ee33f26e656f4
Danial Behzadi <dani.behzi@ubuntu.com>
2017-01-01 11:29:25 +0000

    Translated using Weblate (Persian)
    Currently translated at 100.0% (7 of 7 strings)

--- asteroid-weather.fa.ts
+++ asteroid-weather.fa.ts
@@ -6,39 +6,40 @@
     <message>
         <location filename="main.qml" line="42"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>نامعلوم</translation>
     </message>
     <message>
         <location filename="main.qml" line="75"/>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>امروز</translation>
     </message>
     <message>
         <location filename="main.qml" line="78"/>
         <source>Tomorrow</source>
-        <translation type="unfinished"></translation>
+        <translation>فردا</translation>
     </message>
     <message>
         <location filename="main.qml" line="120"/>
         <source>Min:</source>
-        <translation type="unfinished"></translation>
+        <translation>کمینه:</translation>
     </message>
     <message>
         <location filename="main.qml" line="120"/>
         <location filename="main.qml" line="142"/>
         <source>°C</source>
-        <translation type="unfinished"></translation>
+        <translation>°س</translation>
     </message>
     <message>
         <location filename="main.qml" line="142"/>
         <source>Max:</source>
-        <translation type="unfinished"></translation>
+        <translation>بیشینه:</translation>
     </message>
     <message>
         <location filename="main.qml" line="194"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;بدون داده&lt;/h3&gt;
+استروید اواِس را با تلفنتان هم‌گام کنید.</translation>
     </message>
 </context>
 </TS>
