adea70bf7578e9e2bef142fd0742011ce6ca565d
Muḥend Belqasem <belkacem77@gmail.com>
2017-03-01 14:44:27 +0000

    Translated using Weblate (Kabyle)
    Currently translated at 100.0% (7 of 7 strings)

--- asteroid-weather.kab.ts
+++ asteroid-weather.kab.ts
@@ -6,39 +6,40 @@
     <message>
         <location filename="main.qml" line="42"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Arussin</translation>
     </message>
     <message>
         <location filename="main.qml" line="75"/>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>Ass-a</translation>
     </message>
     <message>
         <location filename="main.qml" line="78"/>
         <source>Tomorrow</source>
-        <translation type="unfinished"></translation>
+        <translation>Azekka</translation>
     </message>
     <message>
         <location filename="main.qml" line="120"/>
         <source>Min:</source>
-        <translation type="unfinished"></translation>
+        <translation>Addday:</translation>
     </message>
     <message>
         <location filename="main.qml" line="120"/>
         <location filename="main.qml" line="142"/>
         <source>°C</source>
-        <translation type="unfinished"></translation>
+        <translation>°C</translation>
     </message>
     <message>
         <location filename="main.qml" line="142"/>
         <source>Max:</source>
-        <translation type="unfinished"></translation>
+        <translation>Afellay:</translation>
     </message>
     <message>
         <location filename="main.qml" line="194"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Ulac isefka&lt;/h3&gt;
+Mtawi AsteroidOS akked tiliɣri inek.</translation>
     </message>
 </context>
 </TS>
