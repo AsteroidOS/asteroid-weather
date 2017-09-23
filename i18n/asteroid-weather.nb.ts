71bc64abdc6b1c9c79d53c8d2ef553d23da53fab
Allan Nordhøy <epost@anotheragency.no>
2017-09-23 08:34:05 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (8 of 8 strings)

--- i18n/asteroid-weather.nb.ts
+++ i18n/asteroid-weather.nb.ts
@@ -6,39 +6,40 @@
     <message>
         <location filename="asteroid-weather.desktop.h" line="5"/>
         <source>Weather</source>
-        <translation type="unfinished"></translation>
+        <translation>Vær</translation>
     </message>
 </context>
 <context>
     <name>main</name>
     <message>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Ukjent</translation>
     </message>
     <message>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>I dag</translation>
     </message>
     <message>
         <source>Tomorrow</source>
-        <translation type="unfinished"></translation>
+        <translation>I morgen</translation>
     </message>
     <message>
         <source>Min:</source>
-        <translation type="unfinished"></translation>
+        <translation>Min:</translation>
     </message>
     <message>
         <source>°C</source>
-        <translation type="unfinished"></translation>
+        <translation>°C</translation>
     </message>
     <message>
         <source>Max:</source>
-        <translation type="unfinished"></translation>
+        <translation>Maks:</translation>
     </message>
     <message>
         <source>&lt;h3&gt;No data&lt;/h3&gt;
 Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Ingen dato&lt;/h3&gt;
+Synkroniser AsteroidOS med telefonen din.</translation>
     </message>
 </context>
 </TS>
