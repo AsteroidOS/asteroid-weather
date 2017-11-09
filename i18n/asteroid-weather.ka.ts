1770590e9cbb0b6f38f6a1e841fe90af5f260422
sam bowlby <sam@chven.us>
2017-11-09 06:47:55 +0000

    Added translation using Weblate (Georgian)

---
+++ i18n/asteroid-weather.ka.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ka">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="asteroid-weather.desktop.h" line="5"/>
+        <source>Weather</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Unknown</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Today</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Tomorrow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Min:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>°C</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Max:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;No data&lt;/h3&gt;
+Sync AsteroidOS with your phone.</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
