c6df013caaeb3917d54c536af95c4d0748210f21
Minori Hiraoka (미노리) <minori@mnetwork.co.kr>
2018-04-09 20:45:46 +0000

    Translated using Weblate (Korean)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-weather.ko.ts
+++ i18n/asteroid-weather.ko.ts
@@ -21,7 +21,7 @@
     <message id="id-no-data-sync">
         <location filename="../main.qml" line="89"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation>&lt;h3&gt;데이터 없음&lt;/h3&gt; AsteroidOS를 귀하의 폰과 연동시키세요.</translation>
+        <translation>&lt;h3&gt;데이터 없음&lt;/h3&gt;AsteroidOS를 귀하의 폰과 연동시키세요.</translation>
     </message>
     <message id="id-min">
         <location filename="../main.qml" line="145"/>
