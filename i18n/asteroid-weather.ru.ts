5725aa0777c459c522ac02273fb4950c05865c8c
DaniilKivenko <rostovslipknot@gmail.com>
2018-05-15 21:02:52 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-weather.ru.ts
+++ i18n/asteroid-weather.ru.ts
@@ -36,7 +36,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-weather.desktop.h" line="6"/>
         <source>Weather</source>
-        <translation>погода</translation>
+        <translation>Погода</translation>
     </message>
 </context>
 </TS>
