9e5bdbfa9ea152da36b1fc10a370bf140a64986e
Goudarz Jafari <goudarz.jafari@gmail.com>
2018-05-16 13:04:03 +0000

    Translated using Weblate (Persian)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-weather.fa.ts
+++ i18n/asteroid-weather.fa.ts
@@ -26,17 +26,17 @@
     <message id="id-min">
         <location filename="../main.qml" line="145"/>
         <source>Min:</source>
-        <translation>کمینه:</translation>
+        <translation>حداقل:</translation>
     </message>
     <message id="id-max">
         <location filename="../main.qml" line="163"/>
         <source>Max:</source>
-        <translation>بیشینه:</translation>
+        <translation>حداکثر:</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-weather.desktop.h" line="6"/>
         <source>Weather</source>
-        <translation>ﻮﻀﻋ ﻩﻭﺍ</translation>
+        <translation>آب و هوا</translation>
     </message>
 </context>
 </TS>
