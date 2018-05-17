128ac0e0532ce4ed5aa1159eae4604888e01450e
Mika Beckerich <beckerichmika@gmail.com>
2018-05-17 19:21:01 +0000

    Added translation using Weblate (Luxembourgish)

---
+++ i18n/asteroid-weather.lb.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lb">
+<context>
+    <name></name>
+    <message id="id-today">
+        <location filename="../main.qml" line="49"/>
+        <source>Today</source>
+        <translation></translation>
+    </message>
+    <message id="id-tomorrow">
+        <location filename="../main.qml" line="53"/>
+        <source>Tomorrow</source>
+        <translation></translation>
+    </message>
+    <message id="id-unknown">
+        <location filename="../main.qml" line="84"/>
+        <source>Unknown</source>
+        <translation></translation>
+    </message>
+    <message id="id-no-data-sync">
+        <location filename="../main.qml" line="89"/>
+        <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
+        <translation></translation>
+    </message>
+    <message id="id-min">
+        <location filename="../main.qml" line="145"/>
+        <source>Min:</source>
+        <translation></translation>
+    </message>
+    <message id="id-max">
+        <location filename="../main.qml" line="163"/>
+        <source>Max:</source>
+        <translation></translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-weather.desktop.h" line="6"/>
+        <source>Weather</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
