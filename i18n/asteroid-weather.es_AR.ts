32215da089405058fa859cf8d8d116c9a7f606ad
Rodrigo Bernardi <rodrigobernardi@outlook.com.ar>
2018-05-17 16:53:15 +0000

    Translated using Weblate (Spanish (Argentina))
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-weather.es_AR.ts
+++ i18n/asteroid-weather.es_AR.ts
@@ -6,37 +6,37 @@
     <message id="id-today">
         <location filename="../main.qml" line="49"/>
         <source>Today</source>
-        <translation></translation>
+        <translation>Hoy</translation>
     </message>
     <message id="id-tomorrow">
         <location filename="../main.qml" line="53"/>
         <source>Tomorrow</source>
-        <translation></translation>
+        <translation>Mañana</translation>
     </message>
     <message id="id-unknown">
         <location filename="../main.qml" line="84"/>
         <source>Unknown</source>
-        <translation></translation>
+        <translation>Desconocido</translation>
     </message>
     <message id="id-no-data-sync">
         <location filename="../main.qml" line="89"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation></translation>
+        <translation>&lt;h3&gt;No hay datos&lt;/h3&gt;Sincroniza AsteroidOS con tu teléfono.</translation>
     </message>
     <message id="id-min">
         <location filename="../main.qml" line="145"/>
         <source>Min:</source>
-        <translation></translation>
+        <translation>Mín.:</translation>
     </message>
     <message id="id-max">
         <location filename="../main.qml" line="163"/>
         <source>Max:</source>
-        <translation></translation>
+        <translation>Máx.:</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-weather.desktop.h" line="6"/>
         <source>Weather</source>
-        <translation></translation>
+        <translation>Clima</translation>
     </message>
 </context>
 </TS>
