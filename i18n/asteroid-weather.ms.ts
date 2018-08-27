bc3498f2f2f1c88658ff6836ca8937fedfd29877
mohamad farid <mfarid.ahamid@gmail.com>
2018-08-27 06:07:33 +0000

    Translated using Weblate (Malay)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-weather.ms.ts
+++ i18n/asteroid-weather.ms.ts
@@ -6,22 +6,22 @@
     <message id="id-today">
         <location filename="../main.qml" line="49"/>
         <source>Today</source>
-        <translation></translation>
+        <translation>Hari ini</translation>
     </message>
     <message id="id-tomorrow">
         <location filename="../main.qml" line="53"/>
         <source>Tomorrow</source>
-        <translation></translation>
+        <translation>Esok</translation>
     </message>
     <message id="id-unknown">
         <location filename="../main.qml" line="84"/>
         <source>Unknown</source>
-        <translation></translation>
+        <translation>Tidak diketahui</translation>
     </message>
     <message id="id-no-data-sync">
         <location filename="../main.qml" line="89"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation></translation>
+        <translation>&lt;h3&gt;Tiada data&lt;/h3&gt;Segerakkan AsteroidOS dengan telefon anda.</translation>
     </message>
     <message id="id-min">
         <location filename="../main.qml" line="145"/>
@@ -31,12 +31,12 @@
     <message id="id-max">
         <location filename="../main.qml" line="163"/>
         <source>Max:</source>
-        <translation></translation>
+        <translation>Maks:</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-weather.desktop.h" line="6"/>
         <source>Weather</source>
-        <translation></translation>
+        <translation>Cuaca</translation>
     </message>
 </context>
 </TS>
