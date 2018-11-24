3d7abc0fce788151694981ed1703f0eedb830776
ditokp <ditokpl@gmail.com>
2018-11-24 11:44:51 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-weather.id.ts
+++ i18n/asteroid-weather.id.ts
@@ -6,37 +6,37 @@
     <message id="id-today">
         <location filename="../main.qml" line="49"/>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>Hari ini</translation>
     </message>
     <message id="id-tomorrow">
         <location filename="../main.qml" line="53"/>
         <source>Tomorrow</source>
-        <translation type="unfinished"></translation>
+        <translation>Besok</translation>
     </message>
     <message id="id-unknown">
         <location filename="../main.qml" line="84"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Tak Diketahui</translation>
     </message>
     <message id="id-no-data-sync">
         <location filename="../main.qml" line="89"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Tak ada data&lt;/h3&gt; Sinkronisasi AsteroidOS dengan telepon Anda.</translation>
     </message>
     <message id="id-min">
         <location filename="../main.qml" line="145"/>
         <source>Min:</source>
-        <translation type="unfinished"></translation>
+        <translation>Min:</translation>
     </message>
     <message id="id-max">
         <location filename="../main.qml" line="163"/>
         <source>Max:</source>
-        <translation type="unfinished"></translation>
+        <translation>Max:</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-weather.desktop.h" line="6"/>
         <source>Weather</source>
-        <translation type="unfinished"></translation>
+        <translation>Cuaca</translation>
     </message>
 </context>
 </TS>
