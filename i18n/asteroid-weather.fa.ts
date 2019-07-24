82b873c34ab719ee62f9ea4af6c484069671acc2
Danial Behzadi <dani.behzi@ubuntu.com>
2019-07-24 11:02:42 +0000

    Translated using Weblate (Persian)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-weather.fa.ts
+++ i18n/asteroid-weather.fa.ts
@@ -21,22 +21,22 @@
     <message id="id-no-data-sync">
         <location filename="../main.qml" line="89"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation>&lt;h3&gt;بدون داده&lt;/h3&gt;استروید اواِس را با تلفنتان هم‌گام کنید.</translation>
+        <translation>&lt;h3&gt;بدون داده&lt;/h3&gt;استروید را با تلفنتان هم‌گام کنید.</translation>
     </message>
     <message id="id-min">
         <location filename="../main.qml" line="145"/>
         <source>Min:</source>
-        <translation>حداقل:</translation>
+        <translation>کمینه:</translation>
     </message>
     <message id="id-max">
         <location filename="../main.qml" line="163"/>
         <source>Max:</source>
-        <translation>حداکثر:</translation>
+        <translation>بیشینه:</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-weather.desktop.h" line="6"/>
         <source>Weather</source>
-        <translation>آب و هوا</translation>
+        <translation>آب‌وهوا</translation>
     </message>
 </context>
 </TS>
