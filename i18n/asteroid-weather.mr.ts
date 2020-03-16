e79c1d1b718cf55bf9af88fe36bb6f4266496ccd
Prachi Joshi <josprachi@yahoo.com>
2020-03-16 17:33:56 +0000

    Translated using Weblate (Marathi)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-weather.mr.ts
+++ i18n/asteroid-weather.mr.ts
@@ -6,37 +6,37 @@
     <message id="id-today">
         <location filename="../main.qml" line="49"/>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>आज</translation>
     </message>
     <message id="id-tomorrow">
         <location filename="../main.qml" line="53"/>
         <source>Tomorrow</source>
-        <translation type="unfinished"></translation>
+        <translation>उद्या</translation>
     </message>
     <message id="id-unknown">
         <location filename="../main.qml" line="84"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>अज्ञात</translation>
     </message>
     <message id="id-no-data-sync">
         <location filename="../main.qml" line="89"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt; कोणताही डेटा नाही &lt;/h3&gt; आपल्या फोनसह AsteroidOS समक्रमित करा.</translation>
     </message>
     <message id="id-min">
         <location filename="../main.qml" line="145"/>
         <source>Min:</source>
-        <translation type="unfinished"></translation>
+        <translation>किमान:</translation>
     </message>
     <message id="id-max">
         <location filename="../main.qml" line="163"/>
         <source>Max:</source>
-        <translation type="unfinished"></translation>
+        <translation>कमाल:</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-weather.desktop.h" line="6"/>
         <source>Weather</source>
-        <translation type="unfinished"></translation>
+        <translation>हवामान</translation>
     </message>
 </context>
 </TS>
