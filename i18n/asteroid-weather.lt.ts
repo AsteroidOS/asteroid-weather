9918dc89f81a51f3966f363f46fc108d51c27229
Gediminas Murauskas <muziejusinfo@gmail.com>
2021-07-06 13:35:00 +0000

    Translated using Weblate (Lithuanian)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-weather.lt.ts
+++ i18n/asteroid-weather.lt.ts
@@ -21,22 +21,22 @@
     <message id="id-no-data-sync">
         <location filename="../src/main.qml" line="89"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Nėra duomenų&lt;/h3&gt; Sinchronizuokite AsteroidOS su savo telefonu.</translation>
     </message>
     <message id="id-min">
         <location filename="../src/main.qml" line="145"/>
         <source>Min:</source>
-        <translation type="unfinished"></translation>
+        <translation>Min:</translation>
     </message>
     <message id="id-max">
         <location filename="../src/main.qml" line="163"/>
         <source>Max:</source>
-        <translation type="unfinished"></translation>
+        <translation>Max:</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-weather.desktop.h" line="6"/>
         <source>Weather</source>
-        <translation type="unfinished"></translation>
+        <translation>Orai</translation>
     </message>
 </context>
 </TS>
