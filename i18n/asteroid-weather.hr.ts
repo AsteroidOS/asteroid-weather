47b9c092b862434a9f17f378b4b84b07586c0aff
Milo Ivir <mail@milotype.de>
2021-09-11 22:17:45 +0000

    Translated using Weblate (Croatian)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-weather.hr.ts
+++ i18n/asteroid-weather.hr.ts
@@ -21,7 +21,7 @@
     <message id="id-no-data-sync">
         <location filename="../src/main.qml" line="89"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation>&lt;h3&gt;Nema podataka&lt;/h3&gt;Sinkroniziraj AsteroidOS s mobilnim uređajem.</translation>
+        <translation>&lt;h3&gt;Nema podataka&lt;/h3&gt;Sinkroniziraj AsteroidOS s tvojim telefonom.</translation>
     </message>
     <message id="id-min">
         <location filename="../src/main.qml" line="145"/>
@@ -36,7 +36,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-weather.desktop.h" line="6"/>
         <source>Weather</source>
-        <translation>Vremenska prognoza</translation>
+        <translation>Prognoza</translation>
     </message>
 </context>
 </TS>
