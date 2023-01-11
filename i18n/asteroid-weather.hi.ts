be66c7c18df329167fff68ed51509bdf3617b52d
Xyngg <heyabhinav@protonmail.com>
2023-01-11 09:08:24 +0100

    Added translation using Weblate (Hindi)

---
+++ i18n/asteroid-weather.hi.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hi">
+<context>
+    <name></name>
+    <message id="id-today">
+        <location filename="../src/main.qml" line="49"/>
+        <source>Today</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-tomorrow">
+        <location filename="../src/main.qml" line="53"/>
+        <source>Tomorrow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-unknown">
+        <location filename="../src/main.qml" line="84"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-no-data-sync">
+        <location filename="../src/main.qml" line="89"/>
+        <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-min">
+        <location filename="../src/main.qml" line="145"/>
+        <source>Min:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-max">
+        <location filename="../src/main.qml" line="163"/>
+        <source>Max:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-weather.desktop.h" line="6"/>
+        <source>Weather</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
