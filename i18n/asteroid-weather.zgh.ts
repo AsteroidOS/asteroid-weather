17e4a8d3fca20bc1c52ab81faf8a0181ab29d5c5
ⵣⵓⵀⵉⵔ ⴰⵎⴰⵣⵉⵖ ZOUHIR DEHBI <zouhirdehbi56@gmail.com>
2023-02-04 21:43:35 +0000

    Translated using Weblate (Tamazight (Standard Moroccan))
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-weather.zgh.ts
+++ i18n/asteroid-weather.zgh.ts
@@ -6,37 +6,37 @@
     <message id="id-today">
         <location filename="../src/main.qml" line="49"/>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>ⴰⵙⵙ ⴰⴷ</translation>
     </message>
     <message id="id-tomorrow">
         <location filename="../src/main.qml" line="53"/>
         <source>Tomorrow</source>
-        <translation type="unfinished"></translation>
+        <translation>ⴰⵙⴽⴽⴰ</translation>
     </message>
     <message id="id-unknown">
         <location filename="../src/main.qml" line="84"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>ⵓⵔ ⵉⵜⵜⵡⴰⵙⵙⵏ</translation>
     </message>
     <message id="id-no-data-sync">
         <location filename="../src/main.qml" line="89"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;ⵓⵔ ⵍⵍⵉⵏⵜ ⵜⵎⵓⵛⴰ&lt;/h3&gt;ⵎⵢⴰⴽⵓⴷ AsteroidOS ⴰⴽⴷ ⵜⵜⵉⵍⵉⴼⵓⵏ ⵏⵏⴽ.</translation>
     </message>
     <message id="id-min">
         <location filename="../src/main.qml" line="145"/>
         <source>Min:</source>
-        <translation type="unfinished"></translation>
+        <translation>ⴰⵎⴷⵔⵓⵙ:</translation>
     </message>
     <message id="id-max">
         <location filename="../src/main.qml" line="163"/>
         <source>Max:</source>
-        <translation type="unfinished"></translation>
+        <translation>ⴰⵎⵓⵣⵣⵓⵔ:</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-weather.desktop.h" line="6"/>
         <source>Weather</source>
-        <translation type="unfinished"></translation>
+        <translation>ⴰⵏⵣⵡⵉ</translation>
     </message>
 </context>
 </TS>
