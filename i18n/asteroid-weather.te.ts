3c4453d07c63002d80c08c314bba671d78e45ef4
Govind Gopal Yadav <govindgopalgaming@gmail.com>
2025-04-26 21:24:46 +0200

    Translated using Weblate (Telugu)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-weather.te.ts
+++ i18n/asteroid-weather.te.ts
@@ -21,7 +21,7 @@
     <message id="id-no-data-sync">
         <location filename="../src/main.qml" line="89"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished">&lt;h3&gt;డేటా లేదు&lt;/h3&gt;మీ ఫోన్‌తో AsteroidOSని సమకాలీకరించండి</translation>
+        <translation>&lt;h3&gt;డేటా లేదు&lt;/h3&gt;మీ ఫోన్‌తో AsteroidOSని సమకాలీకరించండి.</translation>
     </message>
     <message id="id-min">
         <location filename="../src/main.qml" line="145"/>
