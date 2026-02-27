32f886f1ea0a9e26121d1c728cae5257fa98fa17
Quentin PAGÈS <quentinantonin@free.fr>
2026-02-27 08:49:31 +0100

    Translated using Weblate (Occitan)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-weather.oc.ts
+++ i18n/asteroid-weather.oc.ts
@@ -21,17 +21,17 @@
     <message id="id-no-data-sync">
         <location filename="../src/main.qml" line="89"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;Cap de donada&lt;/h3&gt;Sincronizatz AsteroidOS amb vòstre telefòn.</translation>
     </message>
     <message id="id-min">
         <location filename="../src/main.qml" line="145"/>
         <source>Min:</source>
-        <translation type="unfinished"></translation>
+        <translation>Min :</translation>
     </message>
     <message id="id-max">
         <location filename="../src/main.qml" line="163"/>
         <source>Max:</source>
-        <translation type="unfinished"></translation>
+        <translation>Max :</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-weather.desktop.h" line="6"/>
