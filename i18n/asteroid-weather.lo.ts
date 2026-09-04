f42f0706de13205d1f9486414158474ad426f18c
Bone NI <bounkirdni@gmail.com>
2026-09-04 23:47:03 +0200

    Translated using Weblate (Lao)
    Currently translated at 100.0% (7 of 7 strings)

--- i18n/asteroid-weather.lo.ts
+++ i18n/asteroid-weather.lo.ts
@@ -6,37 +6,37 @@
     <message id="id-today">
         <location filename="../src/main.qml" line="49"/>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation>ມື້ນີ້</translation>
     </message>
     <message id="id-tomorrow">
         <location filename="../src/main.qml" line="53"/>
         <source>Tomorrow</source>
-        <translation type="unfinished"></translation>
+        <translation>ມື້ອື່ນ</translation>
     </message>
     <message id="id-unknown">
         <location filename="../src/main.qml" line="84"/>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>ບໍ່ລະບຸ</translation>
     </message>
     <message id="id-no-data-sync">
         <location filename="../src/main.qml" line="89"/>
         <source>&lt;h3&gt;No data&lt;/h3&gt;Sync AsteroidOS with your phone.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;h3&gt;ບໍ່ມີຂໍ້ມູນ&lt;/h3&gt;ຊິ້ງ AsteroidOS ກັບໂທລະສັບຂອງທ່ານ.</translation>
     </message>
     <message id="id-min">
         <location filename="../src/main.qml" line="145"/>
         <source>Min:</source>
-        <translation type="unfinished"></translation>
+        <translation>ຕ່ຳສຸດ:</translation>
     </message>
     <message id="id-max">
         <location filename="../src/main.qml" line="163"/>
         <source>Max:</source>
-        <translation type="unfinished"></translation>
+        <translation>ສູງສຸດ:</translation>
     </message>
     <message id="id-app-launcher-name">
         <location filename="asteroid-weather.desktop.h" line="6"/>
         <source>Weather</source>
-        <translation type="unfinished"></translation>
+        <translation>ສະພາບອາກາດ</translation>
     </message>
 </context>
 </TS>
